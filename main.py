5dce48de866850d3165dcef855b6e51946d3629d
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-29 18:00:52 +0200

    ahora funciona todo

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from productor import *
 from consumidor import *
-def iniciar():
+
+if __name__ == '__main__':
+    buffer = [] 
+    buffer_size = 5 
+    mutex = threading.Semaphore(1) 
+    full = threading.Semaphore(0) 
+    empty = threading.Semaphore(buffer_size) 
+    produced = 0 
+    consumed = 0 
+
+   
+    p = Productor(buffer, buffer_size, produced, consumed, mutex, full, empty)
+    c = Consumidor(buffer, buffer_size, produced, consumed, mutex, full, empty)
+
+   
+    p.start()
+    c.start()
+
+
+    p.join()
+    c.join()
