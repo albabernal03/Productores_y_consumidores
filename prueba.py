8a5c83552aacc26331cf7a094b0a955f2f1f7c0c
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-29 17:19:17 +0200

    definzmos el semaforo

--- prueba.py
+++ prueba.py
@@ -7,3 +7,14 @@ import time
 #creaamos una lista vacia
 buffer = [] #esto lo hacemos para que el productor pueda agregar elementos a la lista
 buffer_size = 5 #tamaño del buffer
+#creamos un semaforo
+semaphore = threading.Semaphore(0) #inicializamos el semaforo en 0 para que el consumidor no pueda consumir hasta que el productor no haya producido
+#creamos un mutex
+mutex = threading.Semaphore(1) #inicializamos el mutex en 1 para que el productor pueda producir
+#creamos una variable que nos indique el numero de elementos que se han producido
+produced = 0
+#creamos una variable que nos indique el numero de elementos que se han consumido
+consumed = 0
+full = threading.Semaphore(0)
+empty = threading.Semaphore(buffer_size) 
+
